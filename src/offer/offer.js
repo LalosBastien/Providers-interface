52108007662f615f784cf93858d1dcd74171abcc
erwan boehm <erwan.boehm@gmail.com>
2018-02-22 16:02:16 +0100

    deployed ipfs instance on ipfs.cloudapp.net

--- src/offer/offer.js
+++ src/offer/offer.js
@@ -3,17 +3,19 @@
 */
 import React from "react";
 import { Form, Input, InputNumber, DatePicker, Icon, Select, Row, Col, Button, AutoComplete } from 'antd';
+import {ipfs as IPFS} from "eip808-sdk/lib";
+
 // import  from 'eip808-sdk'
 import './offer.css';
 
-import {ipfs as IPFS} from "eip808-sdk/lib";
 
 const FormItem = Form.Item;
 
 class AvailabilityForm extends React.Component {
   constructor() {
     super();
-    const ipfs = new IPFS('localhost', '5001', this);
+    // const ipfs = new IPFS('localhost', '5001', this);
+    const ipfs = new IPFS('ipfs.cloudapp.net', '5001', this);
     this.captureFile = ipfs.captureFile.bind(this);
     this.saveToIpfs = ipfs.saveToIpfs.bind(this);
     this.arrayBufferToString = ipfs.arrayBufferToString.bind(this);
