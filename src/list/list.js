b50c7d12ef50493589272a18a3bc8872baf5368e
erwan boehm <erwan.boehm@gmail.com>
2018-02-23 11:13:27 +0100

    adding offers

--- src/list/list.js
+++ src/list/list.js
@@ -1,6 +1,6 @@
 /**
- * Created by MisterGreen on 22/02/2018.
- */
+* Created by MisterGreen on 22/02/2018.
+*/
 import React from 'react';
 import { Card, Icon, Avatar, Row, Col } from 'antd';
 import './list.css';
@@ -13,57 +13,57 @@ class List extends React.Component{
         level: []
     };
 
+    requestReservation(i) {
+        fetch(`http://localhost:3000/api/requestReservation/${i}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({}),
+        }).then(() => {alert('success requestReservation')}).catch(e=>console.log(e))
+    }
+
     componentDidMount() {
-        
-        this.setState({
-            level: [
-                [0 , {
-                    name: 'offre 1',
-                    type: 'house',
-                    travelers: 2,
-                    beds: 2,
-                    rooms: 2,
-                    parking: 2
+        async function getMetaData(hash) {
+            return await new Promise(function(resolve, reject) {
+                fetch('https://ipfs.io/ipfs/'+hash)
+                .then(response => response.json())
+                .then(data => resolve(data))
+                .catch(() => {
+                    return resolve({});
+                })
+            });
+        }
 
-                }],
-                [1, {
-                    name: 'offre 2',
-                    type: 'appartement',
-                    travelers: 2,
-                    beds: 2,
-                    rooms: 2,
-                    parking: 2
-
-                }],
-                [2, {
-                    name: 'offre 3',
-                    type: 'appartement',
-                    travelers: 2,
-                    beds: 2,
-                    rooms: 2,
-                    parking: 2
-
-                }],
-                [3, {
-                    name: 'offre 4',
-                    type: 'appartement',
-                    travelers: 2,
-                    beds: 2,
-                    rooms: 2,
-                    parking: 2
-
-                }],
-                [4, {
-                    name: 'offre 5',
-                    type: 'appartement',
-                    travelers: 2,
-                    beds: 2,
-                    rooms: 2,
-                    parking: 2
-
-                }]
-            ]
+        fetch('http://127.0.0.1:3000/api/ListAvailabilities')
+        .then((response) => response.json())
+        .then(async (data) => {
+            console.log("response", data);
+            let mapped = [];
+            for (var i = 0; i < data.length; i++) {
+                let meta =  await getMetaData(data[i]._metaDataLink);
+                let orderedMeta = {
+                    name: meta.name,
+                    type: meta.type,
+                    travelers: meta.travelers,
+                    beds: meta.beds,
+                    rooms: meta.beds,
+                    parking: meta.parking,
+                    picture: meta.imageUrl,
+                    beginDate: new Date(parseInt(data[i]._startDateTs)).toLocaleDateString(),
+                    endDate: new Date(parseInt(data[i]._endDateTs)).toLocaleDateString()
+                }
+                mapped.push([i, orderedMeta])
+                this.setState({level: mapped});
+            }
+            console.log("============:::::::  ", mapped);
+        })
+        .catch((error) => {
+            console.error(error);
         });
+
+
+
     }
 
     render(){
@@ -78,59 +78,69 @@ class List extends React.Component{
                                     <Card
                                         style={{ width: '100%' }}
                                         // todo change URL here by item[1].linkPhoto
-                                        cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
-                                        actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
-                                    >{console.log(item ,  "=====" + index)}
-                                        <Meta
+                                        cover={<img alt="example" src={item[1].picture || "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"} />}
+                                        actions={[<Icon type="check" onClick={() => {this.requestReservation(index)}}/>]}
+                                        >
+                                            {/* {console.log(item ,  "=====" + index)} */}
+                                            <Meta
 
-                                            title={item[1].name}
-                                            description={
-                                                <Row>
-                                                    <Col span={12} className="tiles-content-title">
-                                                        <span>Type</span>
-                                                    </Col>
-                                                    <Col span={12} className="tiles-content-value">
-                                                        <span>{item[1].type}</span>
-                                                    </Col>
-                                                    <Col span={12} className="tiles-content-title">
-                                                        <span>Travelers</span>
-                                                    </Col>
-                                                    <Col span={12} className="tiles-content-value">
-                                                        <span>{item[1].travelers}</span>
-                                                    </Col>
-                                                    <Col span={12} className="tiles-content-title">
-                                                        <span>Rooms</span>
-                                                    </Col>
-                                                    <Col span={12} className="tiles-content-value">
-                                                        <span>{item[1].rooms}</span>
-                                                    </Col>
-                                                    <Col span={12} className="tiles-content-title">
-                                                        <span>Beds</span>
-                                                    </Col>
-                                                    <Col span={12} className="tiles-content-value">
-                                                        <span>{item[1].beds}</span>
-                                                    </Col>
-                                                    <Col span={12} className="tiles-content-title">
-                                                        <span>Parking</span>
-                                                    </Col>
-                                                    <Col span={12} className="tiles-content-value">
-                                                        <span>{item[1].parking}</span>
-                                                    </Col>
-                                                </Row>
-                                            }
-                                        />
-                                    </Card>
-                                </Col>
-                            ))
-                        }
-                    </Row>
-                </Col>
-            </Row>
-        );
+                                                title={item[1].name}
+                                                description={
+                                                    <Row>
+                                                        <Col span={12} className="tiles-content-title">
+                                                            <span>Type</span>
+                                                        </Col>
+                                                        <Col span={12} className="tiles-content-value">
+                                                            <span>{item[1].type}</span>
+                                                        </Col>
+                                                        <Col span={12} className="tiles-content-title">
+                                                            <span>Travelers</span>
+                                                        </Col>
+                                                        <Col span={12} className="tiles-content-value">
+                                                            <span>{item[1].travelers}</span>
+                                                        </Col>
+                                                        <Col span={12} className="tiles-content-title">
+                                                            <span>Rooms</span>
+                                                        </Col>
+                                                        <Col span={12} className="tiles-content-value">
+                                                            <span>{item[1].rooms}</span>
+                                                        </Col>
+                                                        <Col span={12} className="tiles-content-title">
+                                                            <span>Beds</span>
+                                                        </Col>
+                                                        <Col span={12} className="tiles-content-value">
+                                                            <span>{item[1].beds}</span>
+                                                        </Col>
+                                                        <Col span={12} className="tiles-content-title">
+                                                            <span>Parking</span>
+                                                        </Col>
+                                                        <Col span={12} className="tiles-content-value">
+                                                            <span>{item[1].parking}</span>
+                                                        </Col>
+                                                        <Col span={12} className="tiles-content-title">
+                                                            <span>Start date</span>
+                                                        </Col>
+                                                        <Col span={12} className="tiles-content-value">
+                                                            <span>{item[1].beginDate}</span>
+                                                        </Col>
+                                                        <Col span={12} className="tiles-content-title">
+                                                            <span>End date</span>
+                                                        </Col>
+                                                        <Col span={12} className="tiles-content-value">
+                                                            <span>{item[1].endDate}</span>
+                                                        </Col>
+                                                    </Row>
+                                                }
+                                            />
+                                        </Card>
+                                    </Col>
+                                ))
+                            }
+                        </Row>
+                    </Col>
+                </Row>
+            );
+        }
     }
-}
 
-export default List;
-
-
-
+    export default List;
